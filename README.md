6a0f7cf22547f4020fe830a9ff4263f2c5c638ec
Adam Byrne <adamrbyrne@gmail.com>
2023-03-22 16:05:22 +0000

    v1

---
+++ README.md
@@ -0,0 +1,28 @@
+# Face Recognition
+---
+
+## Description
+This is a simple face recognition program. 
+Using OpenCV and a Haar Cascade Classifier, it is able to detect faces in a video stream.
+It will save the frame with the most faces in it to a file.
+
+## Setup
+1. Install Dependencies
+	1. OpenCV
+	2. Numpy
+
+    ```bash
+	pip3 install -r requirements.txt
+	```
+
+2. Run the program
+
+	```bash
+	python3 main.py
+	```
+
+## TODO
+- [ ] Add a GUI
+- [ ] Add a way to save the frames to a folder
+- [ ] Add a way to save the frames to a video
+- [ ] Add a way to save the frames to a video with the faces highlighted and the name of the person
--- main.py
+++ main.py
@@ -1,42 +1,43 @@
 import cv2
 
-choice = input("Video or live? (0 or 1): ")
-
-if choice == "0":
-	cap = cv2.VideoCapture(0)
-
-elif choice == "1":
-	cap = cv2.VideoCapture(1)
-else:
-	print("Invalid input, exiting...")
-	exit()
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture("video.mp4") # stock video from pexels
 
 cv2.namedWindow('img', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('img', 640, 480)
 cv2.setWindowTitle('img', 'Face Detection')
 
-while cv2.waitKey(30) & 0xff != 27: 
-	for i in range(2):
+max_faces = 0
+
+while cv2.waitKey(30) & 0xff != 27:
+	for i in range(2): # skip 2 frames
 		_, img = cap.read()
 
 	if img is None: # loop video
 		cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 		continue
 
-	img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	img = cv2.resize(img, fx=0.5, fy=0.5, dsize=None)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to grayscale -> faster processing
+	gray = cv2.GaussianBlur(gray, (5, 5), 0) # blur image -> reduce noise
+	gray = cv2.equalizeHist(gray) # equalize histogram -> improve contrast
 
-	faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-	cv2.putText(img, "Faces: " + str(len(faces)), (10, img.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2, cv2.LINE_AA)
+	faces = detector.detectMultiScale(gray, 1.1, 4)
 
 	for (x, y, w, h) in faces:
-		cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+		cv2.circle(img, (x + w//2, y + h//2), 5, (0, 0, 255), -1)
+		cv2.circle(img, (x + w//2, y + h//2), w//2, (0, 0, 255), 2)
+
+	if len(faces) > max_faces:
+		max_faces = len(faces)
+
+	cv2.putText(img, f'Faces: {len(faces)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+	cv2.putText(img, f'Max Faces: {max_faces}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+	if len(faces) == max_faces:
+		cv2.imwrite('max_faces.jpg', img)
 
 	cv2.imshow('img', img)
 
 cap.release()
-
-
+cv2.destroyAllWindows()
