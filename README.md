b085b17bea1886c4c59d8cd1bc0a6f12ab43e7fc
Adam Byrne <adamrbyrne@gmail.com>
2023-03-22 16:26:25 +0000

    v2

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
-# Face Recognition
----
 
-## Description
-This is a simple face recognition program. 
+# Face Detection
+
+![Demo](max_faces.jpg)
+
+##  Description
+This is a simple face detection program. 
 Using OpenCV and a Haar Cascade Classifier, it is able to detect faces in a video stream.
 It will save the frame with the most faces in it to a file.
 
-## Setup
-1. Install Dependencies
-	1. OpenCV
-	2. Numpy
+## Video
+The video used in the demo is from Pexels, link to file: [https://www.pexels.com/video/video-of-people-walking-855564/](https://www.pexels.com/video/video-of-people-walking-855564/)
 
+##  Setup
+1. Install dependencies
     ```bash
 	pip3 install -r requirements.txt
 	```
@@ -20,8 +22,8 @@ It will save the frame with the most faces in it to a file.
 	```bash
 	python3 main.py
 	```
-
-## TODO
+	
+## TODO
 - [ ] Add a GUI
 - [ ] Add a way to save the frames to a folder
 - [ ] Add a way to save the frames to a video
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import cv2
 
-detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-cap = cv2.VideoCapture("video.mp4") # stock video from pexels
+max_faces = 0
 
 cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('img', 640, 480)
 cv2.setWindowTitle('img', 'Face Detection')
+cv2.resizeWindow('img', 800, 600)
+cv2.moveWindow('img', 0, 0)
 
-max_faces = 0
+
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture("video.mp4") # stock video from pexels
+fps = cap.get(cv2.CAP_PROP_FPS)
+
 
 while cv2.waitKey(30) & 0xff != 27:
 	for i in range(2): # skip 2 frames
@@ -24,15 +28,15 @@ while cv2.waitKey(30) & 0xff != 27:
 
 	faces = detector.detectMultiScale(gray, 1.1, 4)
 
-	for (x, y, w, h) in faces:
-		cv2.circle(img, (x + w//2, y + h//2), 5, (0, 0, 255), -1)
-		cv2.circle(img, (x + w//2, y + h//2), w//2, (0, 0, 255), 2)
 
 	if len(faces) > max_faces:
 		max_faces = len(faces)
 
-	cv2.putText(img, f'Faces: {len(faces)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-	cv2.putText(img, f'Max Faces: {max_faces}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+	for (x, y, w, h) in faces:
+		cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 0), 2)
+
+	cv2.putText(img, f'Faces: {len(faces)}', (10, 525), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+	cv2.putText(img, f'{fps:.2f}fps', (800, 525), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
 	if len(faces) == max_faces:
 		cv2.imwrite('max_faces.jpg', img)
